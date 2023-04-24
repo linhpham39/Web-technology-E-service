af495783e0f92b6bb019631add34491f88be784a
linhpham39 <linhshark02@gmail.com>
2023-04-25 02:56:52 +0700

    Initial App

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Hotel from './pages/hotel/Hotel';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/hotels" element={<List />}/>
+        <Route path="/hotels/:id" element={<Hotel />}/>
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
