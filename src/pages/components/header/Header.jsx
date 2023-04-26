c615ce4fef79e8982adc1c52f6c4ad0f789f4e85
linhpham39 <linhshark02@gmail.com>
2023-04-26 15:14:47 +0700

    update header

--- src/pages/components/header/Header.jsx
+++ src/pages/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBed, faCar, faMountainSun, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
+import { faBed, faCalendar, faCalendarDays, faCar, faMountainSun, faPerson, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
 import "./header.css"
 
 
@@ -34,6 +34,32 @@ function Header() {
                 <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
                 <p className="headerDesc">Get reward for your travels- unlock instant savings of 10% or more with a free LinkBooking</p>
                 <button className="headerBtn">Sign in / Register</button>
+                <div className="headerSearch">
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faCalendar} className='headerIcon' />
+                        <input
+                            type="text"
+                            placeholder='Where are you going?'
+                            className='headerSearchInput' />
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
+                        <span className="headerSearchText">date to date</span>
+                        <input
+                            type="text"
+                            className='headerSearchInput' />
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faPerson} className='headerIcon' />
+                        <span className="headerSearchText">2 aldults 2 children 1 room</span>
+                        <input
+                            type="text"
+                            className='headerSearchInput' />
+                    </div>
+                    <div className="headerSearchItem">
+                        <button className="headerBtn">Search</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
