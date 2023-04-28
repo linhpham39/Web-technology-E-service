f25db4c867166ce6f38886b07bb2ce32b30269a5
linhpham39 <linhshark02@gmail.com>
2023-04-28 15:41:18 +0700

    update header search bar

--- src/pages/components/header/Header.jsx
+++ src/pages/components/header/Header.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBed, faCalendar, faCalendarDays, faCar, faMountainSun, faPerson, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
+import { DateRange } from 'react-date-range'
 import "./header.css"
-
-
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import format from 'date-fns/format'
 
 function Header() {
+    const [openDate, setOpenDate] = useState(false);
+    const [date, setDate] = useState([
+        {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: 'selection'
+        }
+    ]);
+    const [openOptions, setOpenOptions] = useState(false);
+    const [options, setOptions] = useState({
+        adult: 1,
+        children: 0,
+        room: 1
+    })
+
     return (
         <div className='header'>
             <div className="headerContainer">
@@ -44,17 +61,45 @@ function Header() {
                     </div>
                     <div className="headerSearchItem">
                         <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
-                        <span className="headerSearchText">date to date</span>
-                        <input
-                            type="text"
-                            className='headerSearchInput' />
+                        <span
+                            onClick={() => setOpenDate(!openDate)}
+                            className="headerSearchText">
+                            {`${format(date[0].startDate, "MM/dd/yyyy")} to 
+                            ${format(date[0].endDate, "MM/dd/yyyy")}`}
+                        </span>
+                        {openDate && <DateRange
+                            editableDateInputs={true}
+                            onChange={item => setDate([item.selection])}
+                            moveRangeOnFirstSelection={false}
+                            ranges={date}
+                            className='date'
+                        />}
                     </div>
                     <div className="headerSearchItem">
                         <FontAwesomeIcon icon={faPerson} className='headerIcon' />
-                        <span className="headerSearchText">2 aldults 2 children 1 room</span>
-                        <input
-                            type="text"
-                            className='headerSearchInput' />
+                        <span className="headerSearchText">
+                            {`${options.adult} Adult, ${options.children} Children, ${options.room} Room`}
+                        </span>
+                        <div className="options">
+                            <div className="optionItem">
+                                <span className="optionText">Adult</span>
+                                <button className="optionCounterButtun">-</button>
+                                <span className="optionCounterNumber">1</span>
+                                <button className="optionCounterButton">+</button>
+                            </div>
+                            <div className="optionItem">
+                                <span className="optionText">Children</span>
+                                <button className="optionCounterButtun">-</button>
+                                <span className="optionCounterNumber">0</span>
+                                <button className="optionCounterButton">+</button>
+                            </div>
+                            <div className="optionItem">
+                                <span className="optionText">Room</span>
+                                <button className="optionCounterButtun">-</button>
+                                <span className="optionCounterNumber">1</span>
+                                <button className="optionCounterButton">+</button>
+                            </div>
+                        </div>
                     </div>
                     <div className="headerSearchItem">
                         <button className="headerBtn">Search</button>
