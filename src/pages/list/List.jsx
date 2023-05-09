a8760753c7f370a83b5278fc212da3ac5d1cd416
LinhEXOL <tlinhexol@gmail.com>
2023-05-10 05:32:55 +0700

    update

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,15 +1,110 @@
-import React from 'react'
-import Navbar from '../components/navbar/Navbar'
-import Header from '../components/header/Header'
-
+import React, { useState } from "react";
+import "./list.css";
+import Navbar from "../components/navbar/Navbar";
+import Header from "../components/header/Header";
+import { useLocation } from "react-router-dom/dist";
+import format from "date-fns/format";
+import { DateRange } from "react-date-range";
+import SearchItem from "../components/searchItem/SearchItem";
 
 export const List = () => {
+  //location lưu trữ các thông tin chuyển đến từ người dùng
+  // cụ thể chứa destination, date, option đc gửi đến (trong Header)
+  const location = useLocation();
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.date);
+  const [openDate, setopenDate] = useState(false); //để mở lịch or đóng lịch, state = false: đóng trước, khi click vào ms mở ra
+  const [options, setOptions] = useState(location.state.options);
+
   return (
     <div>
       <Navbar></Navbar>
-      <Header type='list'></Header>
+      <Header type="list"></Header>
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
+              <label>Destination</label>
+              <input placeholder={destination} type="text" />
+            </div>
+            <div className="lsItem">
+              <label>Check-in Date</label>
+              <span onClick={() => setopenDate(!openDate)}>
+                {`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
+                  date[0].endDate,
+                  "MM/dd/yyyy"
+                )}`}
+              </span>
+              {openDate && (
+                <DateRange
+                  onChange={(item) => setDate([item.selection])}
+                  minDate={new Date()} //không thể chọn ngày cũ hơn ngày hôm nay
+                  ranges={date} // để kéo từ ngày ... đến ngày ...
+                />
+              )}
+            </div>
+            <div className="lsItem">
+              <label>Options</label>
+              <div className="lsOptions">
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Min price <small> per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Max price <small> per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Adult</span>
+                  <input
+                    type="number"
+                    min={1}
+                    className="lsOptionInput"
+                    placeholder={options.adult}
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Children</span>
+                  <input
+                    type="number"
+                    min={0}
+                    className="lsOptionInput"
+                    placeholder={options.children}
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Room</span>
+                  <input
+                    type="number"
+                    min={1}
+                    className="lsOptionInput"
+                    placeholder={options.room}
+                  />
+                </div>
+              </div>
+            </div>
+            <button>Search</button>
+          </div>
+          <div className="listResult">
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-
-}
-export default List
+  );
+};
+export default List;
