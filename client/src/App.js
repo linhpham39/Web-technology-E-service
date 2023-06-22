211d1a2f914ade502a4d30f44ee1181cb6feb50b
LinhEXOL <tlinhexol@gmail.com>
2023-06-23 02:44:41 +0700

    Committing local changes before merge

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import {
 import Home from './pages/home/Home';
 import List from './pages/list/List';
 import Hotel from './pages/hotel/Hotel';
-
+import Login from './pages/login/Login';
 import React from 'react'
 
 
@@ -17,7 +17,7 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/hotels" element={<List />}/>
         <Route path="/hotels/:id" element={<Hotel />}/>
-        
+        <Route path="/login" element={<Login/>}/>
         </Routes>
     </BrowserRouter>
   );
--- client/src/pages/components/navbar/Navbar.jsx
+++ client/src/pages/components/navbar/Navbar.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./navbar.css"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+import { AuthContext } from '../../../context/AuthContext'
+
 const Navbar = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className='navbar'>
       <div className="navContainer">
-        <Link to="/"style={{color:"inherit", TextDecoderation: "none" }} >
-        <span className="logo">LINKBOOKING</span>
+        <Link to="/" style={{ color: "inherit", TextDecoderation: "none" }}>
+          <span className='logo'> LINKBOOKING </span>
         </Link>
-        <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>
+        {user ? user.username : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
+          </div>
+        )}
       </div>
     </div>
   )
