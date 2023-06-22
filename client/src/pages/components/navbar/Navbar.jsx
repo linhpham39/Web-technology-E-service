a9803761b80ad42cf6f05bfbcdda7f817ae22744
liinhhnt <linh.nt200349@sis.hust.edu.vn>
2023-06-22 22:50:46 +0700

    complete Login Auth context API

--- client/src/pages/components/navbar/Navbar.jsx
+++ client/src/pages/components/navbar/Navbar.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./navbar.css"
+import { Link } from "react-router-dom"
+import { AuthContext } from '../../../context/AuthContext'
 
 const Navbar = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className='navbar'>
       <div className="navContainer">
-        <span className="logo">LINKBOOKING</span>
-        <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>
+        <Link to="/" style={{ color: "inherit", TextDecoderation: "none" }}>
+          <span className='logo'> LINKBOOKING </span>
+        </Link>
+        {user ? user.username : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
+          </div>
+        )}
       </div>
     </div>
   )
